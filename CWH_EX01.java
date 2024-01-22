5c48bd547167b0c16b41176791695cd8b7fae954
Bhavesh Nankani <bhavesh@Bhaveshs-MacBook-Pro.local>
2024-01-22 23:16:21 +0000

    committed

---
+++ CWH_EX01.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CWH_EX01 {
+    public static void main(String[] args) {
+        System.out.println("Taking input from user: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Subject 1 mark: ");
+        int a = sc.nextInt();
+        System.out.println("Enter subject 2 mark: ");
+        int b = sc.nextInt();
+        System.out.println("Enter subject 3 mark: ");
+        int c = sc.nextInt();
+        System.out.println("Enter subject 4 mark: ");
+        int d = sc.nextInt();
+        System.out.println("Enter subject 5 mark: ");
+        int e = sc.nextInt();
+        
+        int sum = a + b + c + d + e; 
+        int percentage = sum/5;
+        System.out.println(percentage);
+    }
+}
