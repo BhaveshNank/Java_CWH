63e0868060297e23653b919d89d3acd996a07707
Bhavesh Nankani <bhavesh@Bhaveshs-MacBook-Pro.local>
2024-01-23 21:10:48 +0000

    committed

---
+++ CWH_ch1_PS04.java
@@ -0,0 +1,9 @@
+public class CWH_ch1_PS04 {
+    public static void main(String[] args) {
+        float a = 3;
+        float conversion = a * 0.62f;
+        System.out.println(conversion);
+
+
+    }
+}
---
+++ CWH_ch1_PS05.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CWH_ch1_PS05 {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the number: ");
+        Scanner sc = new Scanner(System.in);
+        boolean integer = sc.hasNextInt();
+        System.out.println(integer);
+
+
+    }
+}
